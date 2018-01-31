b551e7fd0d65c6d41593201bac478363e9ded530
Adrián Ribao Martínez <adrian@adrima.es>
2018-01-31 16:06:33 +0100

    Add Spanish Language

---
+++ src/Languages/Spanish.elm
@@ -0,0 +1,71 @@
+module Languages.Spanish exposing (lang)
+
+{-| Spanish language configuration.
+
+@docs lang
+
+-}
+
+import Language exposing (..)
+
+
+spanishOrdinal : Ordinal
+spanishOrdinal number =
+    let
+        number1 =
+            floor number
+
+        b =
+            number1 % 10
+    in
+    if b == 1 || b == 3 then
+        "er"
+    else if b == 2 then
+        "do"
+    else if b == 7 || b == 0 then
+        "mo"
+    else if b == 8 then
+        "vo"
+    else if b == 9 then
+        "no"
+    else
+        "to"
+
+
+{-| Configuration data.
+
+    lang =
+        { delimiters =
+            { thousands = "."
+            , decimal = ","
+            }
+        , abbreviations =
+            { thousand = "k"
+            , million = "mm"
+            , billion = "b"
+            , trillion = "t"
+            }
+        , ordinal = spanishOrdinal
+        , currency =
+            { symbol = "€"
+            }
+        }
+
+-}
+lang : Language
+lang =
+    { delimiters =
+        { thousands = "."
+        , decimal = ","
+        }
+    , abbreviations =
+        { thousand = "k"
+        , million = "mm"
+        , billion = "b"
+        , trillion = "t"
+        }
+    , ordinal = spanishOrdinal
+    , currency =
+        { symbol = "€"
+        }
+    }
